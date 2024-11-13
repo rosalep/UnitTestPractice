bcc05becf3cc19b50df96589a0ed1be6424e53ed
Paulina Rosales <prosales@csuchico.edu>
2024-11-13 11:49:28 -0800

    Password Testing

--- Password.cpp
+++ Password.cpp
@@ -10,8 +10,9 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
-  int repetition = 1;
+  int repetition = 0; // changed from 1 to 0
   int index = 0;
+  
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
     repetition++;
     index++;
@@ -25,5 +26,20 @@ int Password::count_leading_characters(string phrase){
   letter and at least one lower-case letter
 */
 bool Password::has_mixed_case(string pass){
+  int index = 0;
+  bool lower=false;
+  bool upper=false;
+
+  // empty pass 
+  if(pass.length()==0){return false;}
+
+  while( index < pass.length()-1){
+    if (lower && upper){return true;}
+    if(isupper(pass[index])){upper=true;}
+    if(islower(pass[index])){lower=true;}
+    
+    index++;
+  }
   return false;
 }
+
